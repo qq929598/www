203ded5eff9fc91b610d2938cbcc192fde7c3e7d
qq929598 <929598@qq.com>
2020-04-06 21:08:22 +0800

    0406_1.0

--- 0406/js/index.js
+++ 0406/js/index.js
@@ -3,17 +3,18 @@ let shopModule = (function () {
         cardList = document.querySelector('.cardBox'),
         data = null;
 
-    let queryData = function queryData(){
+    let queryData = function queryData() {
         let xhr = new XMLHttpRequest;
-        xhr.open('GET','./json/product.json',false);
-        xhr.onreadystatechange = ()=>{
-            if(xhr.readyState===4&&xhr.status===200){
+        xhr.open('GET', './json/product.json', false);
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4 && xhr.status === 200) {
                 data = JSON.parse(xhr.responseText);
             }
         }
-        xhr.send(null);
-    };
-    let render = function render(){
+        xhr.send(null)
+    }
+
+    let render = function render() {
         let str = ``;
         data.forEach(item => {
             let {
@@ -23,28 +24,49 @@ let shopModule = (function () {
                 time,
                 hot,
                 img
-            }=item;
+            } = item;
             str += `<li>
                         <img src="${img}" alt="">
                         <span>${title}</span>
-                        <span>${price}</span>
-                        <span>${time}</span>
-                        <span>${hot}</span>
+                        <span>价格: ${price.toFixed(2)}</span>
+                        <span>日期: ${time}</span>
+                        <span>销量: ${hot}</span>
                     </li>`
         });
-    };
-    let handle = function handle(){
+        cardList.innerHTML = str;
+    }
 
-    };
-    let clear = function clear(){
+    let handle = function handle() {
+        Array.from(navList).forEach((item)=>{
+            item.flag=-1;
+            item.onclick=function(){
+                this.flag *= -1;
+                clear.call(this);
+                let pai = this.getAttribute("data-pai");
+                data.sort((a,b)=>{
+                    a = String(a[pai]).replace(/-/g,'');
+                    b = String(b[pai]).replace(/-/g,'');
+                    return (a-b)*this.flag;
+                })
+                render()
+            }
+        })
+    }
 
-    };
-
-
+    let clear = function clear() {
+         Array.from(navList).forEach((item) => {
+             if(item !== this){
+                item.flag = -1;
+             }
+         });
+    }
 
     return {
         init() {
-
+            queryData()
+            render()
+            handle()
         }
     }
 })();
+shopModule.init();
